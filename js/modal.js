378ec826b5067f9e8d513579ddba23e03d3da207
Slider666 <112625010+Slider666@users.noreply.github.com>
2023-02-15 19:15:29 +0200

    fixed markUp in filter

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,13 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtnFooter: document.querySelector("[data-modal-open-footer]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtnFooter.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
