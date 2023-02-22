c0811fc8b6fafae449896756bdc41051c7a27dc1
Slider666 <112625010+Slider666@users.noreply.github.com>
2023-02-22 12:20:44 +0200

    Added new slider

--- js/slider.js
+++ js/slider.js
@@ -1,44 +1,55 @@
-const prevBtn = document.querySelector('.carousel-prev-btn');
-const nextBtn = document.querySelector('.carousel-next-btn');
-const carousel = document.querySelector('.carousel');
-const slides = carousel.querySelectorAll('.carousel__img');
-const slideWidth = slides[0].clientWidth;
-const visibleSlidesCount = 3;
-let currentSlide = visibleSlidesCount;
+// const carousel = document.querySelector('.carousel');
+// const slides = carousel.querySelectorAll('.carousel__img');
+// const prevBtn = document.querySelector('.carousel__prev-btn');
+// const nextBtn = document.querySelector('.carousel__next-btn');
 
-// Обновляет порядок слайдов в карусели
-function updateSlidesOrder() {
-  const slides = carousel.querySelectorAll('.carousel__img');
-  for (let i = 0; i < slides.length; i++) {
-    let slideOrder = i - currentSlide;
-    if (slideOrder < 0) {
-      slideOrder += slides.length;
-    }
-    slides[i].style.order = slideOrder;
+// const slideWidth = slides[0].clientWidth;
+// const visibleSlidesCount = 5;
+// let currentSlide = visibleSlidesCount;
+
+// function updateSlidesOrder() {
+//   for (let i = 0; i < slides.length; i++) {
+//     let slideOrder = i - currentSlide;
+//     if (slideOrder < 0) {
+//       slideOrder += slides.length;
+//     }
+//     slides[i].style.order = slideOrder;
+//   }
+// }
+
+// function nextSlide() {
+//   currentSlide++;
+//   if (currentSlide >= slides.length) {
+//     currentSlide = 0;
+//   }
+//   updateSlidesOrder();
+// }
+
+// function prevSlide() {
+//   currentSlide--;
+//   if (currentSlide < 0) {
+//     currentSlide = slides.length - 1;
+//   }
+//   updateSlidesOrder();
+// }
+
+// nextBtn.addEventListener('click', nextSlide);
+// prevBtn.addEventListener('click', prevSlide);
+
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   }
-}
-
-// Переключает на следующий слайд
-function nextSlide() {
-  currentSlide++;
-  if (currentSlide >= slides.length) {
-    currentSlide = 0;
-  }
-  updateSlidesOrder();
-}
-
-// Переключает на предыдущий слайд
-function prevSlide() {
-  currentSlide--;
-  if (currentSlide < 0) {
-    currentSlide = slides.length - 1;
-  }
-  updateSlidesOrder();
-}
-
-// Слушатели событий на кнопках управления
-nextBtn.addEventListener('click', nextSlide);
-prevBtn.addEventListener('click', prevSlide);
-
-// Изменяем порядок слайдов, чтобы первоначально показывались видимые слайды
-updateSlidesOrder();
+});
