e58df5c52d1433d970d067ea69c2695d39b21be9
Slider666 <112625010+Slider666@users.noreply.github.com>
2023-03-02 02:47:00 +0200

    Added btnS arrays

--- js/modal.js
+++ js/modal.js
@@ -1,26 +1,21 @@
 const refs = {
-  // Кнопки хедера
   openModalMobileMenuBtn: document.querySelector("[data-modal-mobile-menu-open]"),
-  openModalRegisterBtn: document.querySelector("[data-modal-register-open]"),
-  openModalEnterBtn: document.querySelector('[data-modal-enter-open]'),
+
+  // Кнопки регистрации и входа
+  openModalRegisterBtn: document.querySelectorAll("[data-btn-register-open]"),
+  openModalEnterBtn: document.querySelectorAll('[data-btn-enter-open]'),
   
   // Модальное окно мобилки
   modalMobileMenu: document.querySelector("[data-modal-mobile-menu]"),
-  closeModalMobileMenuBtn: document.querySelector('[data-modal-mobile-menu-close]'),
-  openModalRegisterMobileBtn: document.querySelector("[data-modal-register-open-mobile]"),
-  openModalEnterMobileBtn: document.querySelector('[data-modal-enter-open-mobile]'),
 
   // Модальное окно регистрации
   modalRegister: document.querySelector("[data-modal-register]"),
-  closeModalRegiserBtn: document.querySelector("[data-modal-register-close]"),
 
   // Модальное окно входа
   modalEnter: document.querySelector('[data-modal-enter]'),
-  closeModalEnterBtn: document.querySelector('[data-modal-enter-close]'),
-
-  // Кнопки футера
-  openModalRegisterBtnFooter: document.querySelector("[data-modal-register-open-footer]"),
-  openModalEnterBtnFooter: document.querySelector("[data-modal-enter-open-footer]"),
+  
+  // Кнопки закрытия
+  closeModalBtn: document.querySelectorAll('[data-modal-close]'),
 };
 
 const html = document.documentElement;
@@ -66,7 +61,7 @@ function onCloseModal(modal) {
 // Отрытие и закрытие модального окна мобилки
 // ===========================================================================
   refs.openModalMobileMenuBtn.addEventListener("click", () => onOpenModal(refs.modalMobileMenu));
-  refs.closeModalMobileMenuBtn.addEventListener("click", () => onCloseModal(refs.modalMobileMenu));
+  refs.closeModalBtn.forEach((btn) => btn.addEventListener("click", () => onCloseModal(refs.modalMobileMenu)));
 
   function addClickHandler(openModalMobileBtn, modalMobileMenu, modalWindow) {
     openModalMobileBtn.addEventListener("click", () => {
@@ -75,16 +70,14 @@ function onCloseModal(modal) {
     });
   }
 
-  addClickHandler(refs.openModalRegisterMobileBtn, refs.modalMobileMenu, refs.modalRegister);
-  addClickHandler(refs.openModalEnterMobileBtn, refs.modalMobileMenu, refs.modalEnter);
+  addClickHandler(refs.openModalRegisterBtn[1], refs.modalMobileMenu, refs.modalRegister);
+  addClickHandler(refs.openModalEnterBtn[1], refs.modalMobileMenu, refs.modalEnter);
 
 // ===========================================================================
 // Открытие и закрытие модального окна регистрации и входа
 // ===========================================================================
-refs.openModalRegisterBtn.addEventListener("click", () => onOpenModal(refs.modalRegister));
-refs.openModalRegisterBtnFooter.addEventListener("click", () => onOpenModal(refs.modalRegister));
-refs.closeModalRegiserBtn.addEventListener("click", () => onCloseModal(refs.modalRegister));
+refs.openModalRegisterBtn.forEach((btn) => btn.addEventListener("click", () => onOpenModal(refs.modalRegister)));
+refs.closeModalBtn.forEach((btn) => btn.addEventListener("click", () => onCloseModal(refs.modalRegister)));
 
-refs.openModalEnterBtn.addEventListener("click", () => onOpenModal(refs.modalEnter));
-refs.openModalEnterBtnFooter.addEventListener("click", () => onOpenModal(refs.modalEnter));
-refs.closeModalEnterBtn.addEventListener("click", () => onCloseModal(refs.modalEnter));
+refs.openModalEnterBtn.forEach((btn) => btn.addEventListener("click", () => onOpenModal(refs.modalEnter)));
+refs.closeModalBtn.forEach((btn) => btn.addEventListener("click", () => onCloseModal(refs.modalEnter)));
